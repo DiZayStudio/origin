46912ec5a74af449de3b971a459b96053f717e48
DmitriyZaytsev <79057778281@ya.ru>
2022-11-16 21:35:07 +0300

    Done Lesson3 task_2 ver2

--- Lesson3/task_2/task_2.cpp
+++ Lesson3/task_2/task_2.cpp
@@ -24,7 +24,7 @@ public:
     }
 };
 
-void input_command(Counter& c) {
+void input_command(Counter& count) {
     do {
         char command = ' ';
         std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
@@ -34,41 +34,41 @@ void input_command(Counter& c) {
         if (command == 'х' || command == 'x')
             break;
         else if (command == '+')
-            c.inc();
+            count.inc();
         else if (command == '-')
-            c.dec();
+            count.dec();
         else if (command == '=') {
-            std::cout << c.get_count() << std::endl;
+            std::cout << count.get_count() << std::endl;
         }
     } while (true);
 }
 
-int main()
-{
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
+ Counter setup() {
     std::string input;
     int n = 0;
-
+    
     std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
     std::cin >> input;
     if (input == "да")
     {
         std::cout << "Введите начальное значение счётчика: ";
         std::cin >> n;
-       Counter counter(n);
-       input_command(counter);
+        Counter counter(n);
+        return counter;
     }
     else {
         Counter counter;
-        input_command(counter);
+        return counter;
     }
+}
 
-
-        
-
-       
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     
+    Counter count = setup();
+
+    input_command(count);
 }
 
