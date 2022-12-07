b6ed3f5c2dd39b6dc620204f0bda0b1c07c4ab46
DmitriyZaytsev <79057778281@ya.ru>
2022-12-07 14:13:19 +0300

    Done Lesson9 Task_1

---
+++ Lesson9/task_1/task_1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	bool operator == (Fraction num) {
+		if (this->numerator_ * num.denominator_ == num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+	bool operator != (Fraction num) {
+		if (this->numerator_ * num.denominator_ != num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+	bool operator < (Fraction num) {
+		if (this->numerator_ * num.denominator_ < num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+	bool operator > (Fraction num) {
+		if (this->numerator_ * num.denominator_ > num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+	bool operator <= (Fraction num) {
+		if (this->numerator_ * num.denominator_ <= num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+	bool operator >= (Fraction num) {
+		if (this->numerator_ * num.denominator_ >= num.numerator_ * this->denominator_) return true;
+		else return false;
+	}
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+//	Fraction f2(8, 6);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
