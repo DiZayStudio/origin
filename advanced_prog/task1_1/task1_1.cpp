34a258aa2d6768be619b555a3567e72046ae6716
DmitriyZaytsev <79057778281@ya.ru>
2023-02-16 11:02:01 +0300

    Done Task1_1 and Task1_2

---
+++ advanced_prog/task1_1/task1_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <Windows.h>
+
+std::vector<int> arr{ 4, 7, 9, 14, 12 };
+
+void print() {
+
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Входные данные: ";
+    std::for_each(arr.begin(), arr.end(), [](int num) { std::cout << num << ' '; });
+    std::cout << '\n';
+
+    std::cout << "Выходные данные: ";
+
+    std::for_each(arr.begin(), arr.end(), [](int num) mutable
+        {
+            if (num % 2 != 0) num *= 3;
+    std::cout << num << ' ';
+        });
+}
