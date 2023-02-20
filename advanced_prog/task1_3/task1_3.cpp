ac06e3509d2653d3a80211368c7e0ff07575a0d4
DmitriyZaytsev <79057778281@ya.ru>
2023-02-20 21:10:59 +0300

    Done Task1_3

---
+++ advanced_prog/task1_3/task1_3.cpp
@@ -0,0 +1,40 @@
+﻿#include <functional>
+#include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <math.h>
+
+const double PI = 3.1415926535;
+double grad_to_rad(int x) { return x * PI / 180; }
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double angles[]{ 30, 60, 90 };
+
+
+    std::cout << "[Входные данные]: ";
+    for (const int& angle : angles) {
+        std::cout << angle << " град (" << grad_to_rad(angle) << " рад) ";
+    }
+
+    std::vector<std::function<void(double)>> functions;
+
+    functions.emplace_back([](double angle) {
+        std::cout << "sin: " << std::sin(grad_to_rad(angle)) << " ";
+        });
+
+    functions.emplace_back([](double angle) {
+        std::cout << "cos: " << std::cos(grad_to_rad(angle)) << " ";
+        });
+
+    std::cout << std::endl;
+    std::cout << "[Выходные данные]: " << std::endl;
+
+    for (const auto& angle : angles) {
+        std::cout << angle << ": ";
+        for (const auto& function : functions)
+            function(angle);
+        std::cout << std::endl;
+    }
+}
