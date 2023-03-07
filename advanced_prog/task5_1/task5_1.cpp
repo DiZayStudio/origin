b7fe599f436e6644847ebf67bec2383a5a4d488e
DmitriyZaytsev <79057778281@ya.ru>
2023-03-07 09:57:27 +0300

    Done Task5_2

---
+++ advanced_prog/task5_1/task5_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+template <class T>
+void print_square(T a) {
+    std::cout << "[OUT]: " << a * a;
+}
+
+template <>
+void print_square(std::vector <int> a) {
+    std::cout << "[OUT]:";
+    for(int num : a ) {
+        std::cout << " " << num * num;
+    }
+    
+}
+
+int main()
+{
+    int num = 4;
+    std::cout << "[IN]: " << num << std::endl;
+
+    print_square(num);
+    std::cout << std::endl;
+
+    std::vector<int> v = { -1, 4, 8 };   
+    std::cout << "[IN]:";
+    for (int num : v) {
+        std::cout << " " << num;
+    }
+    std::cout << std::endl;
+
+    print_square(v);
+}
+
---
+++ advanced_prog/task5_2/task5_2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include "Windows.h"
+
+template <class T>
+class table {
+private:
+    T rows;
+    T cols;
+    T** arr;
+
+public:
+    table(T row, T col)
+    {
+        this->rows = row;
+        this->cols = col;
+        arr = new T * [this->rows];
+        for (int i = 0; i < this->rows; i++)
+        {
+            arr[i] = new T[this->cols];
+        }
+    }
+
+    ~table()
+    {
+        for (int i = 0; i < this->rows; i++)
+        {
+            delete[] arr[i];
+        }
+        delete[] arr;
+    }
+
+    T* operator[] (T index) {
+        return arr[index];
+    }
+
+    const T* operator[] (T index) const {
+        return arr[index];
+    }
+
+    const int Size() {
+        return this->rows * this->cols;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    auto test = table<int>(2, 3);
+    test[0][0] = 4;
+    std::cout << test[0][0] << std::endl;
+
+    std::cout << "Размер таблицы: " << test.Size() << std::endl;
+}
