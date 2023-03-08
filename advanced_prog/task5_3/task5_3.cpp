5ce35b4bfb7e607897c70e014f982fa178bac435
DmitriyZaytsev <79057778281@ya.ru>
2023-03-08 10:22:53 +0300

    Done Task5_3

---
+++ advanced_prog/task5_3/task5_3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include "Windows.h"
+
+class Functor {
+private:
+    int sum = 0;
+    int count = 0;
+
+public:
+    void operator()(int val) {
+        if (val % 3 == 0) {
+            sum += val;
+            count++;
+        }
+    }
+
+    void printCount() { std::cout << "get_count() = " << count << std::endl; }
+
+    void printSum() { std::cout << "get_sum() = " << sum << std::endl; }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int arr[] = { 4, 1, 3, 6, 25, 54 };
+
+    std::cout << " Исходный массив:" << std::endl;
+
+    Functor fun;
+
+    for (auto num : arr) {
+        std::cout << " " << num;
+        fun(num);
+    }
+    std::cout << std::endl;
+
+    fun.printSum();
+    fun.printCount();
+}
