e039ab8c1a9db9fa7df5dfc4a7265c28b9f079d6
DmitriyZaytsev <79057778281@ya.ru>
2023-03-10 10:26:58 +0300

    Correction Task5_1

--- advanced_prog/task5_1/task5_1.cpp
+++ advanced_prog/task5_1/task5_1.cpp
@@ -2,34 +2,41 @@
 #include <vector>
 
 template <class T>
-void print_square(T a) {
-    std::cout << "[OUT]: " << a * a;
+T square(const T a) {
+    return a * a;
 }
 
-template <>
-void print_square(std::vector <int> a) {
-    std::cout << "[OUT]:";
-    for(int num : a ) {
-        std::cout << " " << num * num;
+template <class T>
+std::vector <T> square(const std::vector <T> a) {
+
+    std::vector<T> temp(a.size());
+
+    for (auto i = 0; i < temp.size(); i++) {
+        temp[i] = a[i]*a[i] ;
     }
-    
+    return temp;
 }
 
 int main()
 {
     int num = 4;
     std::cout << "[IN]: " << num << std::endl;
-
-    print_square(num);
-    std::cout << std::endl;
-
+    std::cout << "[OUT]: ";
+    std::cout << square(num) << std::endl;
+    
     std::vector<int> v = { -1, 4, 8 };   
+    
     std::cout << "[IN]:";
     for (int num : v) {
         std::cout << " " << num;
     }
     std::cout << std::endl;
 
-    print_square(v);
+    std::vector <int> c = square(v);
+
+    std::cout << "[OUT]:";
+    for (int num : c) {
+        std::cout << " " << num;
+    }
 }
 
