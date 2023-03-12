429efe843d0c3fa4a56aab33baf6144e2dff9929
DmitriyZaytsev <79057778281@ya.ru>
2023-03-12 16:53:41 +0300

    Done Task7_1

---
+++ advanced_prog/task7_1/task7_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+
+int main()
+{
+    std::vector<int> v{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (int i : v)
+        std::cout << i << " ";
+    std::cout << "\n";
+
+    std::sort(v.begin(), v.end()); 
+    auto last = std::unique(v.begin(), v.end());
+
+    v.erase(last, v.end());
+
+    std::cout << "[OUT]: ";
+    for (int i : v)
+        std::cout << i << " ";
+    std::cout << "\n";
+}
