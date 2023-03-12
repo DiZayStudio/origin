03df2504e8abbcc5e2a047468b55283e09306a0d
DmitriyZaytsev <79057778281@ya.ru>
2023-03-12 16:36:17 +0300

    Done Task6_2

---
+++ advanced_prog/task6_2/task6_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+#include "Windows.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int size=0;
+    std::set<int> n;
+    int num;
+
+    std::cout << "Введите число вводимых символов: ";
+    std::cin >> size;
+    for (int i = 0; i < size; i++) {
+        std::cin >> num;
+        n.insert(num);
+    }
+
+    std::cout << "[OUT]:" << std::endl;
+
+    for (auto i = n.end(); i != n.begin();) {
+        i--;
+        std::cout << *i << std::endl;
+    }
+}
