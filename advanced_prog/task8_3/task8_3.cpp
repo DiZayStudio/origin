af44dfe790cc7d25eb602973f80579b94ccb8b41
DmitriyZaytsev <79057778281@ya.ru>
2023-03-17 21:10:29 +0300

    Done Task8

---
+++ advanced_prog/task8_3/task8_3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+template<class T>
+class MyPointer
+{
+private:
+	T* ptr;
+
+public:
+	MyPointer(T *ptr) {
+		this->ptr = ptr;
+	};
+	~MyPointer() {
+		delete ptr;
+	};
+
+	T& operator * () {
+		return *ptr;
+	}
+
+	T* release() {
+		return ptr;
+	}
+
+	MyPointer(const MyPointer& T) = delete;
+	MyPointer& operator=(const MyPointer& T) = delete;
+
+
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	MyPointer<int> pointer = new int(155);
+
+	*pointer = 100;
+
+	std::cout << &pointer << std::endl;
+	std::cout << *pointer << std::endl;
+
+	int* num = pointer.release();
+
+	std::cout << *pointer << std::endl;
+	std::cout << num << std::endl;
+	std::cout << *num << std::endl;
+}
+
