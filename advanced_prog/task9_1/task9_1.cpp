b7b52e3a1d1087ff9fa95fa6580c951cc0f4cd77
DmitriyZaytsev <79057778281@ya.ru>
2023-03-19 16:56:48 +0300

    Done Task 9_1

--- advanced_prog/task9_1/task9_1.cpp
+++ advanced_prog/task9_1/task9_1.cpp
@@ -3,17 +3,24 @@
 
 
 template<class T>
-void move_vectors(std::vector<T>* last, std::vector<T>* first) {
-    for (int i = 0; i < *first.size;i++) {
-        *last[i] = *first[i];
-    }
+void move_vectors(std::vector<T>& lhs, std::vector<T>& rhs) {
+    std::vector<T> temp = std::move(lhs);
+    lhs = std::move(rhs);
+    rhs = std::move(temp);
 }
 
 int main()
-{
-    
+{ 
     std::vector <std::string> one = { "test_string1", "test_string2" };
     std::vector <std::string> two;
-    move_vectors(&two, &one);
-    std::cout << "Hello World!\n";
+    move_vectors(one, two);
+
+    std::cout << "one: \t" ;
+    for (auto temp : one)
+        std::cout << temp << "\t";
+    std::cout << std::endl;
+    std::cout << "two: \t";
+    for (auto temp : two)
+        std::cout << temp << "\t";
+
 }
