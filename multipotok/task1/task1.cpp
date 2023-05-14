44dce2e467c137bede7a6c0ccc6edef090d45bdf
DmitriyZaytsev <79057778281@ya.ru>
2023-05-14 17:57:43 +0300

    Done Task6

---
+++ multipotok/task1/task1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <Windows.h>
+#include <chrono>
+
+
+// 1й поток
+void Client(int &countCl, int maxC) {
+
+   while (countCl < maxC) {
+       using namespace std::chrono_literals;
+       std::this_thread::sleep_for(1000ms);
+
+       ++countCl;
+       std::cout << "В очереди: " << countCl << std::endl;
+    }
+}
+// 2й поток
+void Operator(int &countCl) {
+    while (countCl > 0) {
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(2000ms);
+
+        --countCl;
+        std::cout << "В очереди: " << countCl << std::endl;
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int countClients = 1;
+    int maxCount = 10;
+
+    auto start = std::chrono::steady_clock::now();
+    
+    std::cout << "Количество ядер: " << std::thread::hardware_concurrency() << std::endl;
+
+    std::thread f1(Client, std::ref(countClients), maxCount);
+    std::thread f2(Operator, std::ref(countClients));
+    f1.join();
+    f2.join();
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> el_time = end - start;
+
+    std::cout << el_time.count() << " ms." << std::endl;
+}
--- sql/task6/task6.cpp
+++ sql/task6/task6.cpp
@@ -2,8 +2,9 @@
 #include <Wt/Dbo/Dbo.h>
 #include <Wt/Dbo/backend/Postgres.h>
 #include <Windows.h>
-#pragma execution_character_set("utf-8")
+#include <corecrt_wstring.h>
 
+#pragma execution_character_set("utf-8")
 
 const std::string CONNECTION_STR = "host=localhost port=5432 dbname=postgres user=postgres password=12345678";
 
@@ -36,6 +37,7 @@ public:
     {
         Wt::Dbo::field(a, title, "title");
         Wt::Dbo::belongsTo(a, publisher, "publisher");
+        Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "book");
     }
 };
 
@@ -50,7 +52,6 @@ public:
     {
         Wt::Dbo::field(a, name, "name");
         Wt::Dbo::hasMany(a, stocks, Wt::Dbo::ManyToOne, "shop");
-
     }
 };
 
@@ -72,13 +73,13 @@ public:
     }
 };
 
-
 class Sale
 {
 public:
     int count;
     std::string date;
     Wt::Dbo::ptr<Stock> stock;
+    int price;
 
     template<typename Action>
     void persist(Action& a)
@@ -86,8 +87,10 @@ public:
         Wt::Dbo::field(a, count, "count");
         Wt::Dbo::field(a, date, "date");
         Wt::Dbo::belongsTo(a, stock, "stock");
+        Wt::Dbo::field(a, price, "price");
     }
 };
+
 
 int main(){
 SetConsoleCP(CP_UTF8);
@@ -111,20 +114,17 @@ try {
         Wt::Dbo::Transaction transaction{ session };
 
         std::unique_ptr<Publisher> p1 = std::make_unique<Publisher>();
-        p1->name = "Machaon";
-
+        p1->name = "Drofa";
         std::unique_ptr<Book> b1 = std::make_unique<Book>();
-        b1->title = "Harry Potter";
-
+        b1->title = "Отцы и дети";
         std::unique_ptr<Shop> s1 = std::make_unique<Shop>();
-        s1->name = "Bookstore on Fontanka";
-
+        s1->name = "Labirint";
         std::unique_ptr<Stock> st1 = std::make_unique<Stock>();
         st1->count = 100;
-
         std::unique_ptr<Sale> sl1 = std::make_unique<Sale>();
         sl1->count = 10;
-        sl1->date = "2023-04-25";
+        sl1->date = "2023-05-09";
+        sl1->price = 525;
 
         auto publisher_added = session.add(std::move(p1));
         auto book_added = session.add(std::move(b1));
@@ -133,21 +133,85 @@ try {
         auto sale_added = session.add(std::move(sl1));
 
         book_added.modify()->publisher = publisher_added;
-
         stock_added.modify()->book = book_added;
-
         shop_added.modify()->stocks.insert(stock_added);
-
         sale_added.modify()->stock = stock_added;
-
         transaction.commit();
+
+        b1 = std::make_unique<Book>();
+        b1->title = "Мастер и маргарита";
+        st1 = std::make_unique<Stock>();
+        st1->count = 2000;
+        sl1 = std::make_unique<Sale>();
+        sl1->count = 40;
+        sl1->date = "2023-05-14";
+        sl1->price = 685;
+
+        book_added = session.add(std::move(b1));
+        stock_added = session.add(std::move(st1));
+        sale_added = session.add(std::move(sl1));
+
+        book_added.modify()->publisher = publisher_added;
+        stock_added.modify()->book = book_added;
+        shop_added.modify()->stocks.insert(stock_added);
+        sale_added.modify()->stock = stock_added;
+        transaction.commit();
+
+        p1 = std::make_unique<Publisher>();
+        p1->name = "Drofa";
+        b1 = std::make_unique<Book>();
+        b1->title = "Преступление и наказание";
+        st1 = std::make_unique<Stock>();
+        st1->count = 200;
+        sl1 = std::make_unique<Sale>();
+        sl1->count = 50;
+        sl1->date = "2023-05-11";
+        sl1->price = 386;
+
+        book_added = session.add(std::move(b1));
+        stock_added = session.add(std::move(st1));
+        sale_added = session.add(std::move(sl1));
+
+        book_added.modify()->publisher = publisher_added;
+        stock_added.modify()->book = book_added;
+        shop_added.modify()->stocks.insert(stock_added);
+        sale_added.modify()->stock = stock_added;
+        transaction.commit();
+
+
+        p1 = std::make_unique<Publisher>();
+        p1->name = "Просвящение";
+        b1 = std::make_unique<Book>();
+        b1->title = "Мастер и маргарита";
+        s1 = std::make_unique<Shop>();
+        s1->name = "Chitay gorod";
+        st1 = std::make_unique<Stock>();
+        st1->count = 1000;
+
+        sl1 = std::make_unique<Sale>();
+        sl1->count = 60;
+        sl1->date = "2023-05-12";
+        sl1->price = 285;
+
+        publisher_added = session.add(std::move(p1));
+        book_added = session.add(std::move(b1));
+        shop_added = session.add(std::move(s1));
+        stock_added = session.add(std::move(st1));
+        sale_added = session.add(std::move(sl1));
+
+        book_added.modify()->publisher = publisher_added;
+        stock_added.modify()->book = book_added;
+        shop_added.modify()->stocks.insert(stock_added);
+        sale_added.modify()->stock = stock_added;
+        transaction.commit();
+
     }
     catch (const Wt::Dbo::Exception& e)
     {
-        std::cout << "Failed to create tables: " << e.what() << std::endl;
+        std::cout << "Не удалось создать таблицы: " << e.what() << std::endl;
     }
 
-    std::cout << "Введите автора: ";
+    std::cout << "Введите издательство: ";
     std::string publisherName;
 
     std::cin >> publisherName;
@@ -158,33 +222,22 @@ try {
 
     if (publisher)
     {
-        std::cout << "Автор найден: " << publisher->name << std::endl;
+        std::cout << "Издательство: " << publisher->name << std::endl;
 
         Wt::Dbo::collection<Wt::Dbo::ptr<Book>> books = publisher->books;
 
         std::vector<Wt::Dbo::ptr<Stock>> stocks;
         for (auto book : books)
         {
+            std::cout << "Книга: " << book->title;
             std::for_each(book->stocks.begin(), book->stocks.end(), [&stocks](Wt::Dbo::ptr<Stock> s) {
-                stocks.push_back(s);
+                std::cout << " в магазине: " << s->shop->name << std::endl;
             });
         }
-
-        for (auto stock : stocks)
-        {
-            std::cout << "Stock found: " << stock->book->title << " in shop: " << stock->shop->name << std::endl;
-        }
-
-        // Other way:
-        using ResultType = std::tuple< Wt::Dbo::ptr<Stock>, Wt::Dbo::ptr<Book> >;
-
-        ResultType query = session.query<ResultType>("select stock, book from stock").join<Book>("book", "book.id = stock.book_id").limit(1);
-
-        std::cout << std::get<0>(query)->shop->name << " " << std::get<1>(query)->title << std::endl;
     }
     else
     {
-        std::cout << "Автор не найден" << std::endl;
+        std::cout << "Издательство не найдено" << std::endl;
     }
 
     t1.commit();
@@ -193,7 +246,6 @@ catch (const Wt::Dbo::Exception& e)
 {
     std::cout << e.what() << std::endl;
 }
-
 
 return 0;
 }
