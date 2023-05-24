81271e8a363250fe3d05f89fefc06db11181f6d7
DmitriyZaytsev <79057778281@ya.ru>
2023-05-24 20:54:01 +0300

    Done Task2_3

---
+++ multipotok/task2_2/task2_2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <Windows.h>
+#include <chrono>
+#include <mutex>
+
+std::mutex m;
+int* sum;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::vector<int> x1;
+
+    sum = 0;
+    // Длина расчёта
+    const long n = 10000;
+    // Количество потоков
+    int n_thread = 4;
+
+    for (int i = 0; i < n; i++) {
+        x1.push_back(i);
+    }
+
+    std::vector<std::thread> thr;
+    using namespace std::chrono_literals;
+
+    for (int i = 0; i < n_thread; i++) {
+        thr.push_back(std::thread([=]() {
+            
+            auto start = std::chrono::steady_clock::now();
+
+            m.lock();
+
+            std::cout << "Номер потока: " << i << std::endl;
+            std::cout << "Идентификатор потока: " << std::this_thread::get_id << std::endl;
+
+            int n1 = (i) * n / n_thread;
+            int n2 = (i+1)*n / n_thread - 1;
+            int delta = (n2 - n1) / 20;
+
+            std::cout << "[";
+            for (int j = n1; j < n2; j++) {
+                    sum += x1[j];
+
+                if (j % delta == 0) {
+                    std::cout << "=";
+                }
+                std::this_thread::sleep_for(2us);
+            }
+            std::cout << "]" << std::endl;
+            auto end = std::chrono::steady_clock::now();
+            std::chrono::duration<double, std::milli> el_time = end - start;
+
+            std::cout << "Время работы потока: " << el_time.count() << " ms." << std::endl;
+
+            std::cout << std::endl;
+            m.unlock();
+        }
+            ));
+    }
+
+    for (auto& f : thr) {
+        f.join();
+    }
+}
---
+++ multipotok/task2_3/task2_3.cpp
@@ -0,0 +1,125 @@
+﻿#include <chrono>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+#include <Windows.h>
+using namespace std::chrono_literals;
+
+
+class Data{
+private:
+    int n;
+    double d;
+    bool b;
+public:
+    Data(int n, double d, bool b) {
+        this->n = n;
+        this->d = d;
+        this->b = b;
+    }
+    void Set_n(int n) {
+        this->n = n;
+    }
+    void Set_d(double d) {
+        this->d = d;
+    }
+    void Set_b(bool b) {
+        this->b = b;
+    }
+
+    int Get_n() {
+        return this->n;
+    }
+    double Get_d() {
+        return this->d;
+    }
+    bool Get_b() {
+        return this->b;
+    }
+
+    void print() {
+        std::cout << "n = " << n << ", d = " << d << ", b = " << b << std::endl;
+    }
+    
+    std::mutex m;
+};
+
+void swap1(Data& a, Data& b) {
+
+    std::lock_guard<std::mutex> lm1{ a.m };
+    std::lock_guard<std::mutex> lm2{ b.m };
+
+    int tmp = a.Get_n();
+    a.Set_n(b.Get_n());
+    b.Set_n(tmp);
+
+    double tmp1 = a.Get_d();
+    a.Set_d(b.Get_d());
+    b.Set_d(tmp1);
+
+    bool tmp2 = a.Get_b();
+    a.Set_b(b.Get_b());
+    b.Set_b(tmp2);
+}
+
+void swap2(Data &a, Data &b) {
+
+    std::scoped_lock sm(a.m, b.m);
+
+    int tmp = a.Get_n();
+    a.Set_n(b.Get_n());
+    b.Set_n(tmp);
+
+    double tmp1 = a.Get_d();
+    a.Set_d(b.Get_d());
+    b.Set_d(tmp1);
+
+    bool tmp2 = a.Get_b();
+    a.Set_b(b.Get_b());
+    b.Set_b(tmp2);
+}
+
+void swap3(Data& a, Data& b) {
+
+    std::unique_lock lock1{ a.m, std::defer_lock };
+    std::unique_lock lock2{ b.m, std::defer_lock };
+
+    std::lock(lock1, lock2);
+
+    int tmp = a.Get_n();
+    a.Set_n(b.Get_n());
+    b.Set_n(tmp);
+
+    double tmp1 = a.Get_d();
+    a.Set_d(b.Get_d());
+    b.Set_d(tmp1);
+
+    bool tmp2 = a.Get_b();
+    a.Set_b(b.Get_b());
+    b.Set_b(tmp2);
+}
+
+int main()
+{
+    Data x1(1, 3.14, true), x2(2, 6.28, false);
+    x1.print();
+    x2.print();
+
+    swap1(x1, x2);
+
+    x1.print();
+    x2.print();
+
+    swap2(x1, x2);
+
+    x1.print();
+    x2.print();
+
+    swap3(x1, x2);
+
+    x1.print();
+    x2.print();
+}
