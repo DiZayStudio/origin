c83523c298a7907c97d04e0140010c430f440943
DmitriyZaytsev <79057778281@ya.ru>
2023-07-11 21:58:47 +0300

    Done task3_3

--- shabloni/task3_3/task3_3.cpp
+++ shabloni/task3_3/task3_3.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
+#include <fstream>
 
 enum Type {
-    WARNING,
-    ERROR,
     FATAL,
+    ERROR,
+    WARNING,
     UNKNOWN
 };
 
@@ -11,36 +12,81 @@ class LogMessage {
 private:
     Type _type;
     std::string _message;
-  //  LogMessage& nextLogMessage;
+    LogMessage*  _nextLogMessage;
 public:
-    LogMessage(Type type, std::string message) : _message(message), _type(type) {}
+    LogMessage(Type type, std::string message) : _message(message), _type(type) {
+        _nextLogMessage = nullptr;
+    }
     Type get_type() const { return _type; }
     const std::string& get_message() const { return _message; }
-};
-
-
-
-class LogUnknownMsg : public LogMessage {
-public:
-    LogUnknownMsg(std::string message) : LogMessage(UNKNOWN, message) {}
-};
-
-class LogWarningMsg : public LogMessage {
-public:
-    LogWarningMsg(std::string message) : LogMessage(WARNING, message) {}
-};
-
-class LogErrorMsg : public LogMessage {
-public:
-    LogErrorMsg(std::string message) : LogMessage(ERROR, message) {}
+    void addNextLogMessage(LogMessage*  nextLogMessage) {
+        this->_nextLogMessage = nextLogMessage;
+    }
+    LogMessage* GetNextLog() {
+        return _nextLogMessage;
+    }
+    virtual void Execute(Type type, std::string message) = 0;
 };
 
 class LogFatalMsg : public LogMessage {
 public:
     LogFatalMsg(std::string message) : LogMessage(FATAL, message) {}
+    void Execute(Type type, std::string message) {
+        if (this->get_type() == FATAL) {
+            throw "Фатальная ошибка";
+        }
+        if (this->GetNextLog() != nullptr) {
+            this->GetNextLog()->Execute(type, message);
+        }
+    };
+};
+
+class LogErrorMsg : public LogMessage {
+public:
+    LogErrorMsg(std::string message) : LogMessage(ERROR, message) {}
+    void Execute(Type type, std::string message) {
+        if (this->get_type() == ERROR) {
+            std::ofstream out_file;
+            out_file.open("log.txt", std::ios::app);
+            if (out_file.is_open())
+            {
+                out_file << message << std::endl;
+            }
+            out_file.close();
+        }
+        if (this->GetNextLog() != nullptr) {
+            this->GetNextLog()->Execute(type, message);
+        }
+    };
+};
+
+class LogWarningMsg : public LogMessage {
+public:
+    LogWarningMsg(std::string message) : LogMessage(WARNING, message) {}
+    void Execute(Type type, std::string message) {
+        if (this->get_type() == ERROR) {
+            std::cout << message << std::endl;
+        }
+        if (this->GetNextLog() != nullptr) {
+            this->GetNextLog()->Execute(type, message);
+        }
+    };
+};
+
+class LogUnknownMsg : public LogMessage {
+public:
+    LogUnknownMsg(std::string message) : LogMessage(UNKNOWN, message) {};
+    void Execute(Type type, std::string message) {
+        if (this->get_type() == FATAL) {
+            throw "Неизвестное сообщение";
+        }
+        if (this->GetNextLog() != nullptr) {
+            this->GetNextLog()->Execute(type, message);
+        }
+    };
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
